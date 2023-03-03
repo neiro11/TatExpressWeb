98ad7007b5f4f79b1353362fab7acf4be8159e66
ildar <ildar.sitdikov.5555@mail.ru>
2023-03-03 19:09:41 +0300

    Страница товара

---
+++ Pages/TovarPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class TovarPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
