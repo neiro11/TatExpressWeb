7fbc3226856681aaa39232b7307701b27db57146
ildar <ildar.sitdikov.5555@mail.ru>
2023-06-07 09:16:42 +0300

    update ful

---
+++ Model/TodoItemDTO.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Model
+{
+
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
